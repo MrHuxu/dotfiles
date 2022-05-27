4c6986de89c20a6b79235d67a48ee67d28a5e0a0
xhu <hxtheone@gmail.com>
2022-05-27 09:00:05 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,10 @@ alias proxy 'export all_proxy=http://127.0.0.1:1087'
 alias nasproxy 'export all_proxy=http://192.168.0.101:1087'
 alias unproxy 'set --erase all_proxy'
 
-set -x GOROOT /Users/xhu/.go/goroot
-set -x GOPATH /Users/xhu/.go/gopath
+set -gx nvm_default_version v18.2.0
+
+set -x GOROOT /Users/xu.hu/.go/goroot
+set -x GOPATH /Users/xu.hu/.go/gopath
 set -x GO111MODULE on
 set -x GOPROXY https://goproxy.cn,https://goproxy.io,direct
 set -x GOSUMDB off
@@ -19,13 +21,15 @@ set -gx ACK_GINKGO_RC true
 
 set -gx PATH "$HOME/.cargo/bin" $PATH
 
-set -gx JAVA_HOME /Users/xhu/.java/jdk-18.0.1.jdk/Contents/Home
+set -gx JAVA_HOME /Users/xu.hu/.java/jdk-18.0.1.1.jdk/Contents/Home
 set -gx PATH $JAVA_HOME/bin $PATH
-set -gx WORKSPACE /Users/xhu/Workspace
+set -gx WORKSPACE /Users/xu.hu/Workspace
 
-set -gx MAVEN_HOME /Users/xhu/.java/apache-maven-3.8.5
+set -gx MAVEN_HOME /Users/xu.hu/.java/apache-maven-3.8.5
 set -gx PATH $MAVEN_HOME/bin $PATH
 
-set -gx JDTLS_HOME /Users/xhu/.java/jdt-language-server-1.10.0-202204131925
-set -gx PATH $JDTLS_HOME/bin $PATH
+set -gx GRADLE_USER_HOME /Users/xu.hu/.java/gradle-7.4.2
+set -gx PATH $GRADLE_USER_HOME/bin $PATH
 
+set -gx JDTLS_HOME /Users/xu.hu/.java/jdt-language-server-1.10.0-202204131925
+set -gx PATH $JDTLS_HOME/bin $PATH
