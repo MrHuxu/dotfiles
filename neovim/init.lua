968ff8029e4780b402cdd5bc55a7faaf29f33b9d
xhu <hxtheone@gmail.com>
2022-08-24 14:52:35 +0800

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -64,7 +64,7 @@ require 'packer'.startup(
 			'glepnir/lspsaga.nvim',
 			branch = 'main',
 			config = function()
-				local saga = require('lspsaga')
+				local saga = require 'lspsaga'
 				saga.init_lsp_saga {}
 			end
 		})
@@ -111,7 +111,7 @@ end
 
 require 'nvim-tree'.setup {}
 
-require('gitsigns').setup {
+require 'gitsigns'.setup {
 	current_line_blame = true,
 	current_line_blame_opts = {
 		delay = 300,
@@ -119,18 +119,18 @@ require('gitsigns').setup {
 	}
 }
 
-local npairs = require('nvim-autopairs')
-local npairs_rule = require('nvim-autopairs.rule')
+local npairs = require 'nvim-autopairs'
+local npairs_rule = require 'nvim-autopairs.rule'
 npairs.setup {
 	disable_filetype = { 'TelescopePrompt', 'guihua', 'guihua_rust', 'clap_input' },
 }
 npairs.add_rule(npairs_rule('<', '>', 'rust'))
 
-require('indent_blankline').setup {
+require 'indent_blankline'.setup {
 	show_current_context = true,
 }
 
-require('lualine').setup {
+require 'lualine'.setup {
 	options = {
 		theme = 'tokyonight',
 		section_separators = { left = '', right = '' },
@@ -143,7 +143,7 @@ require('lualine').setup {
 	}
 }
 
-require('bufferline').setup {
+require 'bufferline'.setup {
 	options = {
 		mode = 'tabs',
 		separator_style = 'slant',
@@ -161,7 +161,7 @@ require('bufferline').setup {
 
 }
 
-require('Comment').setup {}
+require 'Comment'.setup {}
 
 require 'lspconfig'.sumneko_lua.setup {
 	settings = {
@@ -190,7 +190,6 @@ vim.g.go_doc_popup_window = 1
 require 'rust-tools'.setup {
 	tools = {
 		autoSetHints = true,
-		hover_with_actions = true,
 		inlay_hints = {
 			show_parameter_hints = false,
 			parameter_hints_prefix = '<- ',
@@ -208,8 +207,8 @@ require 'rust-tools'.setup {
 	}
 }
 
-local luasnip = require('luasnip')
-require('luasnip.loaders.from_vscode').lazy_load()
+local luasnip = require 'luasnip'
+require 'luasnip.loaders.from_vscode'.lazy_load()
 
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -253,8 +252,6 @@ require 'cmp'.setup {
 	}
 }
 
-require('which-key').register({});
-
 local lang_test_command = {
 	go = 'GoTest',
 	rust = 'rightbelow vert 80RustTest'
@@ -268,34 +265,34 @@ function _G.execute_test_command()
 	end
 end
 
-keymap('n', 'gt', ':lua execute_test_command()<CR>', keymap_opts)
+keymap('n', 'gt', '<CMD>lua execute_test_command()<CR>', keymap_opts)
 
-keymap('n', '<space>source', ':source ~/.config/nvim/init.lua<cr>', keymap_opts)
-keymap('n', 'nt', ':NvimTreeToggle<cr>', keymap_opts)
-keymap('n', 'ff', ':Telescope find_files<cr>', keymap_opts)
-keymap('n', 'fg', ':Telescope live_grep<cr>', keymap_opts)
-keymap('n', 'nh', ':noh<cr>', keymap_opts)
-keymap('n', 'ft', ':FloatermToggle<cr>', keymap_opts)
-keymap('t', '<Esc>', '<C-\\><C-n>:FloatermToggle<CR>', keymap_opts)
+keymap('n', '<space>source', '<CMD>source ~/.config/nvim/init.lua<CR>', keymap_opts)
+keymap('n', 'nt', '<CMD>NvimTreeToggle<CR>', keymap_opts)
+keymap('n', 'ff', '<CMD>Telescope find_files<CR>', keymap_opts)
+keymap('n', 'fg', '<CMD>Telescope live_grep<CR>', keymap_opts)
+keymap('n', 'nh', '<CMD>noh<CR>', keymap_opts)
+keymap('n', 'ft', '<CMD>FloatermToggle<CR>', keymap_opts)
+keymap('t', '<Esc>', '<C-\\><C-n><CMD>FloatermToggle<CR>', keymap_opts)
 
-keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', keymap_opts)
-keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', keymap_opts)
-keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', keymap_opts)
-keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', keymap_opts)
-keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', keymap_opts)
-keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', keymap_opts)
-keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', keymap_opts)
-keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', keymap_opts)
+keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>', keymap_opts)
+keymap('n', '<C-k>', '<CMD>lua vim.lsp.buf.signature_help()<CR>', keymap_opts)
+keymap('n', '<space>wa', '<CMD>lua vim.lsp.buf.add_workspace_folder()<CR>', keymap_opts)
+keymap('n', '<space>wr', '<CMD>lua vim.lsp.buf.remove_workspace_folder()<CR>', keymap_opts)
+keymap('n', '<space>wl', '<CMD>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', keymap_opts)
+keymap('n', '<space>D', '<CMD>lua vim.lsp.buf.type_definition()<CR>', keymap_opts)
+keymap('n', 'gr', '<CMD>lua vim.lsp.buf.references()<CR>', keymap_opts)
+keymap('n', '<space>f', '<CMD>lua vim.lsp.buf.formatting()<CR>', keymap_opts)
 
-keymap('n', '<space>sp', '<cmd>split<cr>', keymap_opts)
-keymap('n', '<space>vsp', '<cmd>vsplit<cr>', keymap_opts)
+keymap('n', '<space>sp', '<CMD>split<CR>', keymap_opts)
+keymap('n', '<space>vsp', '<CMD>vsplit<CR>', keymap_opts)
 keymap('n', '<C-H>', '<C-W><C-H>', keymap_opts)
 keymap('n', '<C-J>', '<C-W><C-J>', keymap_opts)
 keymap('n', '<C-K>', '<C-W><C-K>', keymap_opts)
-keymap('n', '<C-L>', '<C-W><C-L>', keymap_opts)
+keymap('n', '<c-l>', '<c-w><c-l>', keymap_opts)
 
-keymap('n', '<C-c>', '<Esc>', keymap_opts)
-keymap('i', '<C-c>', '<Esc>', keymap_opts)
+keymap('n', '<c-c>', '<esc>', keymap_opts)
+keymap('i', '<c-c>', '<esc>', keymap_opts)
 keymap('v', '<C-c>', '<Esc>', keymap_opts)
 keymap('s', '<C-c>', '<Esc>', keymap_opts)
 keymap('x', '<C-c>', '<Esc>', keymap_opts)
@@ -303,21 +300,23 @@ keymap('c', '<C-c>', '<Esc>', keymap_opts)
 keymap('o', '<C-c>', '<Esc>', keymap_opts)
 keymap('l', '<C-c>', '<Esc>', keymap_opts)
 
-keymap('n', '<space>d', ':bdelete<CR>', keymap_opts)
-keymap('n', '<space>n', ':tabnew<CR>', keymap_opts)
-keymap('n', '<space>.', ':BufferLineCycleNext<CR>', keymap_opts)
-keymap('n', '<space>,', ':BufferLineCyclePrev<CR>', keymap_opts)
+keymap('n', '<space>d', '<CMD>bdelete<CR>', keymap_opts)
+keymap('n', '<space>n', '<CMD>tabnew<CR>', keymap_opts)
+keymap('n', '<space>.', '<CMD>BufferLineCycleNext<CR>', keymap_opts)
+keymap('n', '<space>,', '<CMD>BufferLineCyclePrev<CR>', keymap_opts)
 
-keymap('n', 'tw', ':Trouble workspace_diagnostics<cr>', keymap_opts)
-keymap('n', 'td', ':Trouble document_diagnostics<cr>', keymap_opts)
+keymap('n', 'tw', '<CMD>Trouble workspace_diagnostics<CR>', keymap_opts)
+keymap('n', 'td', '<CMD>Trouble document_diagnostics<CR>', keymap_opts)
 
-keymap('n', 'ca', ':Lspsaga code_action<CR>', keymap_opts)
-keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', keymap_opts)
+keymap('n', 'ca', '<CMD>Lspsaga code_action<CR>', keymap_opts)
+keymap('n', 'K', '<CMD>Lspsaga hover_doc<CR>', keymap_opts)
 keymap('n', 'gs', '<Cmd>Lspsaga signature_help<CR>', keymap_opts)
-keymap('n', '<space>rn', '<cmd>Lspsaga rename<CR>', keymap_opts)
-keymap('n', 'gp', '<cmd>Lspsaga preview_definition<CR>', keymap_opts)
+keymap('n', '<space>rn', '<CMD>Lspsaga rename<CR>', keymap_opts)
+keymap('n', 'gp', '<CMD>Lspsaga preview_definition<CR>', keymap_opts)
 
-require('which-key').register({
+keymap('v', '<space>cp', '"*y', keymap_opts)
+
+require 'which-key'.register({
 	c = {}, f = {}, g = {}, n = {}, r = {}, t = {},
 })
 
