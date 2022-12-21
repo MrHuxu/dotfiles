b2edf86a41b90f2eb2ad41997b662d5deda8577c
xhu <hxtheone@gmail.com>
2022-12-21 15:05:12 +0800

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -206,36 +206,6 @@ require 'rust-tools'.setup {
 	}
 }
 
-require 'lspconfig'.jdtls.setup {}
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = '/path/to/workspace-root/' .. project_name
-require 'jdtls'.start_or_attach {
-	cmd = {
-		'/Users/xu.hu/.java/jdk-17.0.3.1.jdk/Contents/Home/bin/java',
-		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-		'-Dosgi.bundles.defaultStartLevel=4',
-		'-Declipse.product=org.eclipse.jdt.ls.core.product',
-		'-Dlog.protocol=true',
-		'-Dlog.level=ALL',
-		'-Xms1g',
-		'--add-modules=ALL-SYSTEM',
-		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-		'-jar',
-		'/Users/xu.hu/.java/jdt-language-server-1.14.0-202207211651/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-		'-configuration', '/Users/xu.hu/.java/jdt-language-server-1.14.0-202207211651/config_mac',
-		'-data', workspace_dir
-	},
-	root_dir = require 'jdtls.setup'.find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml' }),
-	settings = {
-		java = {
-		}
-	},
-	init_options = {
-		bundles = {}
-	},
-}
-
 local luasnip = require 'luasnip'
 require 'luasnip.loaders.from_vscode'.lazy_load()
 
